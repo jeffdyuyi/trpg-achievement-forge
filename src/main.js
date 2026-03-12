67fd0ea881c736c1a72698367ab6517ad33ec9fa
jeffyuyi <442348584@qq.com>
2026-03-12 12:26:58 +0800

    chore: 清理调试代码，恢复专业版界面

--- src/main.js
+++ src/main.js
@@ -5,10 +5,6 @@ import router from './router'
 import './style.css'
 
 const app = createApp(App)
-app.config.errorHandler = (err) => {
-    document.body.innerHTML = `<div style="padding:20px; color:red;">运行时错误: ${err.message}</div>`;
-    console.error(err);
-}
 app.use(createPinia())
 app.use(router)
 app.mount('#app')
